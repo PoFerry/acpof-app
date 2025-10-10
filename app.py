adade8941bf2205a8c2495cf0e18a249d045e32b
PoFerry <46385026+PoFerry@users.noreply.github.com>
2025-10-09 21:42:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -199,6 +199,27 @@ def ensure_db():
         )""")
 
         # Lignes ingrédients de la recette
+                conn.execute("""
+        CREATE TABLE IF NOT EXISTS recipes(
+            recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE,
+            type TEXT,
+            yield_qty REAL,
+            yield_unit INTEGER,
+            sell_price REAL,
+            FOREIGN KEY(yield_unit) REFERENCES units(unit_id)
+        )""")
+
+        # Texte / méthode de la recette
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS recipe_texts(
+            text_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            recipe_id INTEGER,
+            instructions TEXT,
+            FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id)
+        )""")
+
+        # Lignes ingrédients de la recette
         conn.execute("""
         CREATE TABLE IF NOT EXISTS recipe_lines(
             line_id INTEGER PRIMARY KEY AUTOINCREMENT,
